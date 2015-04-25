34e3612f8623da119ab693cbc4f4be1e13e0ec74
Luis <luis.tavarez@upr.edu>
2015-04-25 16:25:32 -0400

    Tile

---
+++ AI_Project/src/Tile.java
@@ -0,0 +1,15 @@
+
+public class Tile {
+	private Resource resource = null;
+	private boolean isOccupied = false;
+	private int quantity = 0;
+	
+	public Resource extract() {
+		
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+}
