466be6d94daca2fa9e59881e0c518dd53da76c3e
Luis <luis.tavarez@upr.edu>
2015-04-25 20:55:07 -0400

    Added comments

--- AI_Project/src/Map.java
+++ AI_Project/src/Map.java
@@ -10,16 +10,24 @@ public class Map {
 		// TODO Auto-generated constructor stub
 		width = x;
 		height = y;
-		maxRes = (int) Math.sqrt(x*y)/2;
+		maxRes = (int) Math.sqrt(x*y)/2; // Maximum number per resource
 		map = new Tile[x][y];
 	}
 	
+	/**
+	 * Creates random resources in the map, distributes equal number of
+	 * resources maxRes through all resource types	 
+	 */
 	public void randomize() {
 		for(int i=0; i<3; i++) {
 			for(int j=0;j<maxRes;j++) {
 				int randomW = rand.nextInt(width);
 				int randomH = rand.nextInt(height);
 				
+				/*
+				 * Checks if random position has a resource, if it does
+				 * try again, else puts the resource in the random position
+				 */
 				if(check(randomW, randomH)) {
 					j--;
 				} else {
@@ -29,30 +37,88 @@ public class Map {
 		}
 	}
 	
+	/**
+	 * Puts single resource on a random tile
+	 */
+	private void randomize(Resource resource) {
+		int randomW;
+		int randomH;
+		/*
+		 * Checks for available tile
+		 */
+		do {
+			randomW = rand.nextInt(width);
+			randomH = rand.nextInt(height);
+		} while(check(randomW, randomH));
+		/*
+		 * Puts resource on selected tile
+		 */
+		putResource(randomW, randomH, resource);
+	}	
+	
+	/**
+	 * Puts resource on x and y coordinates
+	 * @param x
+	 * @param y
+	 * @param resource
+	 */
 	public void putResource(int x, int y, Resource resource) {
 		map[x][y].putResource(resource);
 	}
 	
+	/**
+	 * Checks if coordinates x and y are occupied by a resource
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public boolean check(int x, int y) {
 		return map[x][y].check(); 
 	}
 	
+	/**
+	 * Gets resource from coordinates x and y
+	 * @param x
+	 * @param y
+	 * @return resource
+	 */
 	public Resource getResource(int x, int y) {
 		return map[x][y].getResource();
 	}
-		
+	
+	/**
+	 * Extracts resource from coordinate, if resource is empty,
+	 * creates it on a random tile
+	 * @param x
+	 * @param y
+	 * @return quantity left
+	 */
 	public int extract(int x, int y) {
-		return map[x][y].extract();
+		int qty = map[x][y].extract(); 
+		if(qty == 0) {
+			randomize(getResource(x, y));
+		}
+		return qty;
 	}
 	
+	/**
+	 * Checks if tile at coordinates x and y has an agent 
+	 * @param x
+	 * @param y
+	 * @return boolean
+	 */
 	public boolean isOccupied(int x, int y) {
 		return map[x][y].isOccupied();
 	}
 	
+	/**
+	 * Puts or removes agent from tile at coordinates x and y
+	 * @param x
+	 * @param y
+	 * @param isOccupied
+	 */
 	public void setOccupied(int x, int y, boolean isOccupied) {
 		map[x][y].setOccupied(isOccupied);
 	}
 		
-	
-
 }
--- AI_Project/src/Tile.java
+++ AI_Project/src/Tile.java
@@ -4,28 +4,52 @@ public class Tile {
 	private boolean isOccupied = false;
 	private int quantity = 0;
 	
+	/**
+	 * Creates new resource with quantity 100 on tile
+	 * @param resource
+	 */
 	public void putResource(Resource resource) {
 		this.resource = resource;
 		this.quantity = 100;
 	}
 	
+	/**
+	 * Checks if tile has a resource
+	 * @return boolean
+	 */
 	public boolean check() {
 		return quantity > 0;
 	}
 	
+	/**
+	 * Returns resource in tile
+	 * @return resource
+	 */
 	public Resource getResource() {
 		return resource;
 	}
 		
+	/**
+	 * Extract 10 resource from tile, returns remaining resources
+	 * @return
+	 */
 	public int extract() {
 		quantity = quantity - 10;
 		return quantity;
 	}
 	
+	/**
+	 * Returns true if agent is in tile
+	 * @return isOccupied
+	 */
 	public boolean isOccupied() {
 		return isOccupied;
 	}
 	
+	/**
+	 * Puts or removes agent from tile
+	 * @param isOccupied
+	 */
 	public void setOccupied(boolean isOccupied) {
 		this.isOccupied = isOccupied;
 	}
