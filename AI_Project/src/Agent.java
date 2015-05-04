badc3980c6d68bb0c464caef033263eed807fde5
LexterSeda <Lexter.seda@upr.edu>
2015-05-04 00:54:55 -0400

    final

--- AI_Project/src/Agent.java
+++ AI_Project/src/Agent.java
@@ -71,7 +71,7 @@ public class Agent {
 
 		AB.uploadList(agentsOfShield);
 
-		AB.prunn(getID(), 7);
+		AB.prunn(getID(), 3);
 
 		// TODO do algorithm to search in here
 		ResourceItem closestResource = getBestResource(); // Finds best resource to get
@@ -527,73 +527,15 @@ public class Agent {
 
 
 
-	public int[] AB_pruning(int node, int depth, int alpha, int beta,int step, Agent agentList[]){
-		int[] v = {-1000000,6};
-		Agent agentL[] = agentList.clone();
-		if(agentL[(node+step)%6].lost()) {
-			return agentL[(node+step+1)%6].AB_pruning(node,depth,alpha,beta,step+1,agentL);
-		}
-		else if(depth == 0){
-
-			int turns = (int)(step / 6)+1;
-			v[0] = agentL[(node+step)%6].getTurnScore(agentL[(node+step)%6].getEssentialResQty(),
-					agentL[(node+step)%6].getDesirableResQty(),agentL[(node+step)%6].getLuxuryResQty(),turns,null);
-			v[0] = ((agentL[(node+step)%6].getDesirableResQty()-turns < 0)||
-					(agentL[(node+step)%6].getEssentialResQty()-(3*turns) < 0)||
-					(agentL[(node+step)%6].getLuxuryResQty()<0)?-1000000:v[0]);
-			v[1] = node;
-			return v;
-		}
-		else if(step%6 == 0){
-			v[0] = -1000000;
-			v[1] = 6;
-			for(int i = 0;i < 7;i++){
-				agentL = agentList;
-				if(i==6 || ( (agentL[i].getLuxuryRes().equals(agentL[(node+step)%6].getEssentialRes()) 
-						|| agentL[i].getLuxuryRes().equals(agentL[(node+step)%6].getDesirableRes())) 
-						&& agentL[i].getEssentialRes().equals(agentL[(node+step)%6].getLuxuryRes()))) {
-					if(i != 6 && agentL[(node+step)%6].canTrade() && agentL[i].canTrade()) {
-						agentL[i] = agentL[(node+step)%6].trade(agentL[i],agentL[i].getLuxuryRes());
-					}
-					int vTemp[] = agentL[(node+step)%6].AB_pruning(node,depth-1,alpha,beta,step+1,agentL);
-					v[0] = Math.max(v[0],vTemp[0]);
-					if(alpha<v[0]){
-						alpha = v[0];
-						v[1] = i;
-					}
-					if(beta <= alpha)
-						break;
-				}
-			}
-			return v;
-		}
-
-		else{
-			v[0] = 1000000;
-			v[1] = 6;
-			for(int i = 0;i < 7;i++){
-				agentL = agentList;
-				if(i==6 || ((agentL[i].getLuxuryRes().equals(agentL[(node+step)%6].getEssentialRes())  
-						|| agentL[i].getLuxuryRes().equals(agentL[(node+step)%6].getDesirableRes())) 
-						&& agentL[i].getEssentialRes().equals(agentL[(node+step)%6].getLuxuryRes()))) {
-					if(i != 6 && agentL[(node+step)%6].canTrade() && agentL[i].canTrade()) {
-						agentL[i] = agentL[(node+step)%6].trade(agentL[i],agentL[i].getLuxuryRes());
-					}
-					int vTemp[] = agentL[(node+step)%6].AB_pruning(node,depth-1,alpha,beta,step+1,agentL);
-					vTemp[0] = vTemp[0] - agentL[node].AB_pruning(node,0,alpha,beta,step,agentL)[0];
-					v[0] = Math.min(v[0],vTemp[0]);
-					if(beta>v[0]){
-						beta = v[0];
-						v[1] = i;
-					}
-					if(beta <= alpha)
-						break;
-				}
-			}
-			return v;
-		}
-	}
-
+	/**
+	 * Distributes the resources for the start of the game
+	 * @param essential Indicates the Essential Resource
+	 * @param essQty Indicates the Essential Resource Quantity
+	 * @param desirable Indicates the Desirable Resource
+	 * @param desQty Indicates the Desirable Resource Quantity
+	 * @param luxury Indicates the Luxury Resource
+	 * @param luxQty Indicates the Luxury Resource Quantity
+	 */
 	private void setResources(Resource essential, int essQty, Resource desirable, int desQty, Resource luxury, int luxQty) {
 
 		essentialRes = essential;
@@ -606,6 +548,9 @@ public class Agent {
 		luxuryResQty = luxQty;
 	}
 
+	/**
+	 * Makes a shallow copy of an Agent
+	 */
 	public Agent clone() {
 		Agent agentClone = new Agent(getID());
 		agentClone.setCoordinates(xCoor, yCoor);
